62d269a22628784ba025d3a73de1d252d10060ef
LukyVj <lucas.bonomi@gmail.com>
2013-12-15 18:01:54 +0100

    update docs

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,19 @@
-console.log('This would be the main JS file.');
+$(document).ready(function(){
+    var NavTop = $('nav').offset().top;
+
+$(window).scroll(function(){
+  if( $(window).scrollTop() > NavTop ) {
+    $('nav').css({position: 'fixed', top: '0px',left: '0px',paddingLeft:'3em',paddingBottom:'1em', background: 'rgba(255,255,255,.8)', borderBottom:"2px solid #1B9AF7"}).find('.btn').css('margin-top','1em');
+  } else {
+    $('nav').css({position: 'static',borderBottom:"0px",paddingLeft:'0em'});
+  }
+});
+
+    
+$('nav a').click(function(e) {
+  e.preventDefault();
+  $('html,body').animate({ 
+    scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+});
+});
